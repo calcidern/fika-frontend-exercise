c1f34783b43ea47b7207f830c2dde569c61639ad
calcidern <kadadel@gmail.com>
2020-10-04 19:10:19 +0100

    tiddy up

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,20 +3,17 @@ import { Genre } from './Genre';
 import { getPosterUrl } from '../services/movieService';
 import './MovieCard.css';
 
-export const MovieCard = ({ title, overview, genres, posterPath }) => {
-  console.log(genres);
-  return (
-    <div className='movie-card'>
-      <img className='movie-card__poster' src={getPosterUrl(posterPath)} alt={title} />
-      <div className='movie-card__description'>
-        <div className='movie-card__title'>{title}</div>
-        <div className='movie-card__genres'>
-          {genres.map(genre => <Genre key={genre} genre={genre} />)}
-        </div>
-        <div className='moview-card__overview'>
-          {overview}
-        </div>
+export const MovieCard = ({ title, overview, genres, posterPath }) => (
+  <div className='movie-card'>
+    <img className='movie-card__poster' src={getPosterUrl(posterPath)} alt={title} />
+    <div className='movie-card__description'>
+      <div className='movie-card__title'>{title}</div>
+      <div className='movie-card__genres'>
+        {genres.map(genre => <Genre key={genre} genre={genre} />)}
+      </div>
+      <div className='moview-card__overview'>
+        {overview}
       </div>
     </div>
-  )
-}
+  </div>
+);
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { MovieCard } from './MovieCard';
 
 
-export const MovieList = ({ movies }) => {
-  return (
-    <div className='movie-list'>
-      {movies.map(({ title, overview, genres, poster_path }) => (
-        <div className='movie-list__item' key={title}>
-          <MovieCard key={title} title={title} overview={overview} genres={genres} posterPath={poster_path}/>
-        </div>
-      ))}
-    </div>
+export const MovieList = ({ movies }) => (
+  <div className='movie-list'>
+    {movies.map(({ title, overview, genres, poster_path }) => (
+      <div className='movie-list__item' key={title}>
+        <MovieCard key={title} title={title} overview={overview} genres={genres} posterPath={poster_path} />
+      </div>
+    ))}
+  </div>
 
-  )
-}
+);
