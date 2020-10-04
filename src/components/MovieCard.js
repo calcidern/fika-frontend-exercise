9749b1fec1dd4e3797607b43d9f42952d16ff325
calcidern <kadadel@gmail.com>
2020-10-04 19:01:22 +0100

    added card styling

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Genre } from './Genre';
+import './MovieCard.css';
 
 export const MovieCard = ({ title, overview, genres }) => {
+  console.log(genres);
   return (
     <div className='movie-card'>
       <div className='movie-card__title'>{title}</div>
       <div className='movie-card__genres'>
-        {genres.map(genere => <Genre key={genere} genre={genres} />)}
+        {genres.map(genre => <Genre key={genre} genre={genre} />)}
       </div>
       <div className='moview-card__overview'>
         {overview}
